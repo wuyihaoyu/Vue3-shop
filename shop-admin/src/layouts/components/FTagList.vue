3646fec47374ca498bfec01b2ec299d9a0e6a53b
林浩裕 <1490227844@qq.com>
2023-03-09 11:19:00 +0800

    标签导航组件实现；关闭其他/全部标签功能；代码拆减封装

--- shop-admin/src/layouts/components/FTagList.vue
+++ shop-admin/src/layouts/components/FTagList.vue
@@ -1,16 +1,15 @@
 <template>
-  <div class="f-tag-list" :style="{left:$store.state.asideWidth}">
+  <div class="f-tag-list" :style="{ left: $store.state.asideWidth }">
     <el-tabs
       v-model="activeTab"
       type="card"
       class="demo-tabs"
       @tab-remove="removeTab"
-      style="min-width: 100px;"
-     
+      style="min-width: 100px"
     >
       <el-tab-pane
         v-for="item in tabList"
-        :closable="item.path !='/'"
+        :closable="item.path != '/'"
         :key="item.path"
         :label="item.title"
         :name="item.path"
@@ -19,138 +18,67 @@
       </el-tab-pane>
     </el-tabs>
     <span class="tag-btn">
-      <el-dropdown>
+      <el-dropdown @command="handleClose">
         <span class="el-dropdown-link">
-          <el-icon >
+          <el-icon>
             <arrow-down />
           </el-icon>
         </span>
         <template #dropdown>
           <el-dropdown-menu>
-            <el-dropdown-item>Action 1</el-dropdown-item>
-            <el-dropdown-item>Action 2</el-dropdown-item>
-            <el-dropdown-item>Action 3</el-dropdown-item>
-            <el-dropdown-item disabled>Action 4</el-dropdown-item>
-            <el-dropdown-item divided>Action 5</el-dropdown-item>
+            <el-dropdown-item command="clearOther">关闭其他</el-dropdown-item>
+            <el-dropdown-item command="clearAll">全部关闭</el-dropdown-item>
           </el-dropdown-menu>
         </template>
       </el-dropdown>
     </span>
   </div>
-  <div style="height:44px;"></div>
+  <div style="height: 44px"></div>
 </template>
 
 <script  setup>
-import { ref } from "vue";
-import{ useRoute,onBeforeRouteUpdate} from "vue-router"
-import { useCookies} from '@vueuse/integrations/useCookies'
+import { useTabList} from "~/composables/useTabList.js"
 
-
-const cookie = useCookies()
-const route = useRoute()
-
-const activeTab = ref(route.path);
-const tabList = ref([
-  {
-    title: "后台首页",
-    path:"/",
- 
-  },
-  {
-    title:"商品管理",
-    path:"/goods/list",
-  }
- 
-]);
-
-function addTab(tab){
-    let noTab = tabList.value.findIndex(t=>t.path ==tab.path)==-1
-     if(noTab){
-        tabList.value.push(tab)
-     }
-     cookie.set("tabList",tabList.value)
-}
-
-function initTabList(){
-    let tbs = cookie.get("tabList")
-    if(tbs){
-        tabList.value = tbs
-    }
-}
-
-initTabList()
-
-onBeforeRouteUpdate((to,from)=>{
-  activeTab.value = to.path
-   addTab( 
-    {
-        title:to.meta.title,
-        path:to.path
-
-    }
-   )
-})
-
-const changeTab =(t)=>{
-    activeTab.value = t
-    router.push(t)
-
-}
-
-const removeTab = (t) => {
-    let a = activeTab.value
-    let tabs = tabList.value
-
-    if(a == t){
-        tabs.forEach((tab,index)=>{
-            if(tab.path==t){
-                const nextTab = tabs[index+1] || tabs[index-1]
-                if(nextTab){
-                   a = nextTab.path
-                }
-            }
-
-        })
-    }
-    activeTab.value = a
-    tabList.value = tabList.value.filter(tab=>tab.path != t)
-    cookie.set("tabList",tabList.value)
-};
+const { activeTab,
+        tabList,
+        changeTab,
+        removeTab,
+        handleClose } = useTabList()
 </script>
 
 <style scoped>
-.f-tag-list{
-    @apply fixed bg-gray-100 flex items-center px-2;
-    top:64px;
-    right:0;
-    height: 44px;
-    z-index: 100;
+.f-tag-list {
+  @apply fixed bg-gray-100 flex items-center px-2;
+  top: 64px;
+  right: 0;
+  height: 44px;
+  z-index: 100;
 }
 
-.tag-btn{
-    @apply bg-white rounded ml-auto flex items-center justify-center px-2;
-    height: 32px;   
+.tag-btn {
+  @apply bg-white rounded ml-auto flex items-center justify-center px-2;
+  height: 32px;
 }
 
-:deep(.el-tabs__header){
-    border:0 !important;
-    @apply mb-0;
+:deep(.el-tabs__header) {
+  border: 0 !important;
+  @apply mb-0;
 }
 
-:deep(.el-tabs__nav){
-    border: 0 !important;
-    margin-top: 4px;
+:deep(.el-tabs__nav) {
+  border: 0 !important;
+  margin-top: 4px;
 }
 
-:deep(.el-tabs__item){
-    border: 0 !important;
-    height: 32px;
-    line-height: 32px;
-    @apply bg-white mx-1 rounded;
+:deep(.el-tabs__item) {
+  border: 0 !important;
+  height: 32px;
+  line-height: 32px;
+  @apply bg-white mx-1 rounded;
 }
 
-:deep(.is-disabled){
-    cursor: not-allowed;
-    @apply  text-gray-300;
+:deep(.is-disabled) {
+  cursor: not-allowed;
+  @apply text-gray-300;
 }
 </style>
