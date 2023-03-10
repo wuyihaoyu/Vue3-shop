cfabcacdde0916188b3c9d71958f275d5f55d6af
林浩裕 <1490227844@qq.com>
2023-03-10 15:03:03 +0800

    统计面板组件开发-1

--- shop-admin/src/App.vue
+++ shop-admin/src/App.vue
@@ -2,12 +2,15 @@
 </script>
 
 <template>
-     <router-view></router-view>
+  <router-view></router-view>
 </template>
 
 <style>
-   #nprogress .bar{
-     background-color: #f4f4f4 !important;
-     height: 3px !important;
-   }
+body {
+  @apply bg-gray-100;
+}
+#nprogress .bar {
+  background-color: #f4f4f4 !important;
+  height: 3px !important;
+}
 </style>
