fb6a072a05691fc06c9225467e276df9739e33a1
林浩裕 <1490227844@qq.com>
2023-03-10 19:22:24 +0800

    2

--- shop-admin/src/components/IndexChart.vue
+++ shop-admin/src/components/IndexChart.vue
@@ -46,35 +46,32 @@ var myChart = null;
 onMounted(() => {
   var chartDom = document.getElementById("chart");
   myChart = echarts.init(chartDom);
-  getData()
-
+  getData();
 });
 
-
-function getData(){
+function getData() {
   var option;
 
-option = {
-  xAxis: {
-    type: 'category',
-    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-  },
-  yAxis: {
-    type: 'value'
-  },
-  series: [
-    {
-      data: [120, 200, 150, 80, 70, 110, 130],
-      type: 'bar',
-      showBackground: true,
-      backgroundStyle: {
-        color: 'rgba(180, 180, 180, 0.2)'
-      }
-    }
-  ]
-};
+  option = {
+    xAxis: {
+      type: "category",
+      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: "bar",
+        showBackground: true,
+        backgroundStyle: {
+          color: "rgba(180, 180, 180, 0.2)",
+        },
+      },
+    ],
+  };
 
-option && myChart.setOption(option);
-
+  option && myChart.setOption(option);
 }
 </script>
