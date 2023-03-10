9931188936ff0389adeebf287b13a2b802d358b5
林浩裕 <1490227844@qq.com>
2023-03-11 01:45:20 +0800

    店铺和交易提示组件的开发与交互

--- shop-admin/src/pages/index.vue
+++ shop-admin/src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-row :gutter="20">
-      <template v-if="(panels.length == 0)">
+      <template v-if="panels.length == 0">
         <el-col :span="6" v-for="i in 4" :key="i">
           <el-skeleton style="width: 100%" animated loading>
             <template #template>
@@ -66,30 +66,43 @@
       </el-col>
     </el-row>
 
-    <IndexNavs/>
+    <IndexNavs />
 
-    <el-row :gutter="20">
+    <el-row :gutter="20" class="mt-5">
       <el-col :span="12" :offset="0">
-       <IndexChart />
+        <IndexChart />
       </el-col>
       <el-col :span="12" :offset="0">
+        <IndexCard title="店铺及商品提示" tip="店铺及商品提示" :btns="goods"  class="mb-3"/>
+        <IndexCard
+          title="交易提示"
+          tip="需要立即处理的交易订单"
+          :btns="order"
+        />
       </el-col>
     </el-row>
-
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import { getStatistics1 } from "~/api/index.js";
-import CountTo from "~/components/CountTo.vue"
-import IndexNavs from "~/components/IndexNavs.vue"
-import IndexChart from "~/components/IndexChart.vue"
-
+import { getStatistics1, getStatistics2 } from "~/api/index.js";
+import CountTo from "~/components/CountTo.vue";
+import IndexNavs from "~/components/IndexNavs.vue";
+import IndexChart from "~/components/IndexChart.vue";
+import IndexCard from "~/components/IndexCard.vue";
 
 const panels = ref([]);
 getStatistics1().then((res) => {
   console.log(res);
   panels.value = res.panels;
+});
+
+const goods = ref([]);
+const order = ref([]);
+
+getStatistics2().then((res) => {
+  goods.value = res.goods;
+  order.value = res.order;
 });
 </script>
