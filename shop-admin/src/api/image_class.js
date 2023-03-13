3ed1818b2f8cc72771d6492bb0a8a0f733aa2510
林浩裕 <1490227844@qq.com>
2023-03-13 15:30:19 +0800

    图库分类列表分页实现-56

---
+++ shop-admin/src/api/image_class.js
@@ -0,0 +1,6 @@
+import axios from "~/axios"
+
+export function getImageClassList(page){
+    return axios.get("/admin/image_class/"+page)
+}
+
--- shop-admin/src/components/ImageAside.vue
+++ shop-admin/src/components/ImageAside.vue
@@ -1,21 +1,44 @@
-
-import { close } from 'fs';
-
 <template>
-  <el-aside width="220px" class="image-aside">
+  <el-aside width="220px" class="image-aside" v-loading="loading">
     <div class="top">
-        <AsideList>分类标题</AsideList>
-
-        <AsideList active>分类标题</AsideList>
+      <AsideList
+        :active="activeId == item.id"
+        v-for="(item, index) in list"
+        :key="index"
+        >{{ item.name }}</AsideList
+      >
     </div>
     <div class="bottom">分页区域</div>
   </el-aside>
 </template>
 
 <script setup>
-import AsideList from '~/components/AsideList.vue'
+import { ref } from "vue";
+import AsideList from "~/components/AsideList.vue";
+import { getImageClassList } from "~/api/image_class.js";
+
+const loading = ref(false);
+const list = ref([]);
+
+const activeId = ref(0);
 
 
+function getData() {
+  loading.value = true;
+  getImageClassList(1)
+    .then((res) => {
+      list.value = res.list;
+      let item  = list.value[0]
+      if(item){
+        activeId.value = item.id
+      }
+    })
+    .finally(() => {
+      loading.value = false;
+    });
+}
+
+getData();
 </script>
 
 <style>
@@ -41,7 +64,4 @@ import AsideList from '~/components/AsideList.vue'
   right: 0;
   @apply flex items-center justify-center;
 }
-
-
-
 </style>
