bc723102fe0fa822f3d4c4a4d7cd4ee8979f114f
林浩裕 <1490227844@qq.com>
2023-03-13 11:20:32 +0800

    图库分类组件开发-55

---
+++ shop-admin/src/components/ImageAside.vue
@@ -0,0 +1,47 @@
+
+import { close } from 'fs';
+
+<template>
+  <el-aside width="220px" class="image-aside">
+    <div class="top">
+        <AsideList>分类标题</AsideList>
+
+        <AsideList active>分类标题</AsideList>
+    </div>
+    <div class="bottom">分页区域</div>
+  </el-aside>
+</template>
+
+<script setup>
+import AsideList from '~/components/AsideList.vue'
+
+
+</script>
+
+<style>
+.image-aside {
+  border-right: 1px solid #eeeeee;
+  position: relative;
+}
+
+.image-aside .top {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 50px;
+  overflow-y: auto;
+}
+
+.image-aside .bottom {
+  position: absolute;
+  bottom: 0;
+  height: 50px;
+  left: 0;
+  right: 0;
+  @apply flex items-center justify-center;
+}
+
+
+
+</style>
