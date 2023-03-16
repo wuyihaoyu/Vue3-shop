3f9b24b9a5ddc4185bda4ea7c066600d8ddd4cbf
林浩裕 <1490227844@qq.com>
2023-03-16 15:50:44 +0800

    公告新增功能实现-68

---
+++ shop-admin/src/api/notice.js
@@ -0,0 +1,9 @@
+import axios from "~/axios"
+
+export function getNoticeList(page){
+    return axios.get(`/admin/notice/${page}`)
+}
+
+export function createNotice(data){
+    return axios.post("/admin/notice",data)
+}
--- shop-admin/src/pages/notice/list.vue
+++ shop-admin/src/pages/notice/list.vue
@@ -1,6 +1,149 @@
 <template>
-    <div>
-        
-        公告列表
+  <el-card shadow="never" class="border-0">
+    <div class="flex items-center justify-between mb-4">
+      <el-button type="primary" size="small" @click="handleCreate"
+        >新增</el-button
+      >
+      <el-tooltip effect="dark" content="刷新数据" placement="top">
+        <el-button text @click="getData">
+          <el-icon :size="20">
+            <Refresh />
+          </el-icon>
+        </el-button>
+      </el-tooltip>
     </div>
+    <el-table :data="tableData" stripe style="width: 100%" v-loading="loading">
+      <el-table-column prop="title" label="公告标题" />
+      <el-table-column prop="create_time" label="发布时间" width="380" />
+      <el-table-column label="操作" width="180" align="center">
+        <template #default="scope">
+          <el-button type="primary" size="small" text>修改</el-button>
+
+          <el-popconfirm
+            title="是否要删除该公告？"
+            confirmButtonText="确认"
+            cancelButtonText="取消"
+            @confirm="handleDelete(scope.row.id)"
+          >
+            <template #reference>
+              <el-button text type="primary" size="small"
+                >删除
+                <el-icon :size="12"><Close></Close></el-icon>
+              </el-button>
+            </template>
+          </el-popconfirm>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <div class="flex items-center justify-center mt-5">
+      <el-pagination
+        background
+        layout="prev,pager, next"
+        :total="total"
+        :current-page="currentPage"
+        :page-size="limit"
+        @current-change="getData"
+      />
+    </div>
+    <FormDrawer ref="formDrawerRef" title="新增" @submit="handleSubmit">
+      <el-form
+        :model="form"
+        ref="formRef"
+        :rules="rules"
+        label-width="80px"
+        :inline="false"
+      >
+        <el-form-item label="公告标题" prop="title">
+          <el-input v-model="form.title" placeholder="公告标题"></el-input>
+        </el-form-item>
+        <el-form-item label="公告内容" prop="content">
+          <el-input
+            v-model="form.content"
+            placeholder="公告内容"
+            type="textarea"
+            :rows="5"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+    </FormDrawer>
+  </el-card>
 </template>
+
+<script setup>
+import { reactive, ref } from "vue";
+import { getNoticeList, createNotice } from "~/api/notice.js";
+import FormDrawer from "~/components/FormDrawer.vue";
+import { toast } from "~/composables/util";
+
+const tableData = ref([]);
+const loading = ref(false);
+
+const currentPage = ref(1);
+const total = ref(0);
+const limit = ref(10);
+
+function getData(p = null) {
+  if (typeof p == "number") {
+    currentPage.value = p;
+  }
+
+  loading.value = true;
+  getNoticeList(currentPage.value)
+    .then((res) => {
+      tableData.value = res.list;
+      total.value = res.totalCount;
+    })
+    .finally(() => {
+      loading.value = false;
+    });
+}
+
+getData();
+
+const handleDelete = (id) => {};
+
+//表单部分
+const formRef = ref(null);
+const form = reactive({
+  title: "",
+  content: "",
+});
+const rules = {
+  title: [
+    {
+      required: true,
+      message: "公告标题不能为空",
+      trigger: "blur",
+    },
+  ],
+  content: [
+    {
+      required: true,
+      message: "公告内容不能为空",
+      trigger: "blur",
+    },
+  ],
+};
+
+const formDrawerRef = ref(null);
+const handleSubmit = () => {
+  formRef.value.validate((valid) => {
+    if (!valid) return;
+    formDrawerRef.value.showLoading();
+    createNotice(form)
+      .then((res) => {
+        toast("新增成功");
+        getData(1);
+        formDrawerRef.value.close();
+      })
+      .finally(() => {
+        formDrawerRef.value.hideLoading();
+      });
+  });
+};
+
+const handleCreate = () => {
+  formDrawerRef.value.open();
+};
+</script>
