ae84eafc475bc6a39d1bd6c2df7f002de3386e92
林浩裕 <1490227844@qq.com>
2023-03-24 03:13:50 +0800

    98

--- shop-admin/src/api/manager.js
+++ shop-admin/src/api/manager.js
@@ -1,4 +1,5 @@
 import axios from "~/axios";
+import {queryParams} from "~/composables/util"
 
 export function login(username,password){
     return axios.post( "/admin/login",{
@@ -22,13 +23,7 @@ export function updatedpassword(data) {
 }
 
 export function getManagerList(page,query={}) {
-    let q = []
-    for(const key  in query)
-    if(query[key]){
-         q.push(`${key}=${encodeURIComponent(query[key])}`)
-    }
-    let r = q.join("&")
-    r = r?("?"+r):""
+    let r = queryParams(query)
     return axios.get(`/admin/manager/${page}${r}`)
 }
 
